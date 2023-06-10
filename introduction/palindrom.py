88c5da7220aa9fbb9bf610514dacce74052c3885
Larisa Kukarskaya <114278098+LarisaKukarskaya@users.noreply.github.com>
2023-06-10 15:15:53 +0300

    Create palindrom.py

---
+++ introduction/palindrom.py
@@ -0,0 +1,19 @@
+import string
+
+sp = input().lower()
+sp = sp.replace(' ', '')
+
+for p in string.punctuation:
+    if p in sp:
+        sp = sp.replace(p, '')
+i = 0
+flag = 1
+while i < len(sp) // 2:
+    if (sp[i] != sp[len(sp) - i - 1]):
+        flag = 0
+        break
+    i += 1
+if flag == 0:
+    print('False')
+else:
+    print('True')
