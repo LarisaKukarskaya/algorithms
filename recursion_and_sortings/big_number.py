f882f958527434bf0e02870070bd032e3fea9f80
Larisa Kukarskaya <114278098+LarisaKukarskaya@users.noreply.github.com>
2023-09-28 20:56:25 +0300

    Create big_number.py

---
+++ recursion_and_sortings/big_number.py
@@ -0,0 +1,14 @@
+def big_number(n, array):
+	for i in range(n-1):
+		for j in range(0, n-1-i):
+			var1 = array[j] + array[j+1]
+			var2 = array[j+1] + array[j]
+			if var1 < var2:
+				array[j], array[j+1] = array[j+1], array[j]
+	print(''.join(array))
+
+
+if __name__ == '__main__':
+	n = int(input())
+	array = input().split(' ')
+	big_number(n, array)
